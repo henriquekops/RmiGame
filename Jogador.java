b6e13e42301907e838201f512734b41f8952bff1
henriquekops <henriquekops@gmail.com>
2020-09-13 18:19:46 -0300

    add editor config + fix Jogador issue (statics) + reformat code

--- Jogador.java
+++ Jogador.java
@@ -5,29 +5,28 @@ import java.rmi.registry.LocateRegistry;
 
 public class Jogador extends UnicastRemoteObject implements JogadorInterface {
 
-private static int idJogador;
+	private static int idJogador;
 
+	public Jogador() throws RemoteException {
+		this.idJogador = 0;
+	}
 
-    public Jogador() throws RemoteException {
-        this.idJogador = 0;
-    }
+	public void inicia() throws RemoteException {
+		//Chamado pelo servidor em cada instancia do jogador
+		//para comecar o jogo propriamente dito
+	}
 
-    public static void main(String args[]){
-    }
+	public void finaliza() throws RemoteException {
+		//O encerramento de um certo jogador quando
+		//o jogador termina o jogo/o jogo termina o jogador
+	}
 
-    public static void inicia() throws RemoteException{
-        //Chamado pelo servidor em cada instancia do jogador
-        //para comecar o jogo propriamente dito
-    }
+	public void cutuca() throws RemoteException {
+		//O servidor checa o jogador a cada 3s
+		//para ver se ele ainda esta ativo/vivo
+	}
 
-    public static void finaliza() throws RemoteException{
-        //O encerramento de um certo jogador quando
-        //o jogador termina o jogo/o jogo termina o jogador
-    }
-
-    public static void cutuca() throws RemoteException{
-        //O servidor checa o jogador a cada 3s
-        //para ver se ele ainda esta ativo/vivo
-    }
+	public static void main(String [] args) {
+	}
 
 }
--- JogoInterface.java
+++ JogoInterface.java
@@ -2,7 +2,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface JogoInterface extends Remote {
-    public int registra() throws RemoteException;
-    public int joga(int id) throws RemoteException;
-    public int encerra(int id) throws RemoteException;
+	public int registra() throws RemoteException;
+	public int joga(int id) throws RemoteException;
+	public int encerra(int id) throws RemoteException;
 }
