6880d2de4227a911b87395f1037ec7b863a0b0f1
henriquekops <henriquekops@gmail.com>
2020-09-18 00:40:18 -0300

    add first  interaction :0

--- src/Jogo.java
+++ src/Jogo.java
@@ -7,7 +7,6 @@ import java.rmi.RemoteException;
 import java.rmi.server.ServerNotActiveException;
 import java.rmi.server.UnicastRemoteObject;
 import java.rmi.registry.LocateRegistry;
-import java.util.concurrent.Semaphore;
 
 public class Jogo extends UnicastRemoteObject implements JogoInterface {
 
@@ -19,18 +18,25 @@ public class Jogo extends UnicastRemoteObject implements JogoInterface {
 	public Jogo() throws RemoteException {}
 
 	public int registra() {
+		int clientId = -1;
+
 		try {
+			String host = getClientHost();
+			System.out.println(host + " is registering...");
 			if (nextPlayer < numPlayers) {
-				players[nextPlayer] = getClientHost();
-				nextPlayer++;
+				players[nextPlayer] = host;
+				nextPlayer += 1;
+				clientId = nextPlayer;
 			}
-			else {
+			if (nextPlayer >= numPlayers) {
+				System.out.println("game is full!");
 				isFull = true;
 			}
 		} catch (ServerNotActiveException e) {
-			System.out.println("Could not establish connection to client");
+			System.out.println("Could not establish connection to client!");
 		}
-		return nextPlayer;
+
+		return clientId;
 	}
 
 	public int joga(int id) {
@@ -43,9 +49,9 @@ public class Jogo extends UnicastRemoteObject implements JogoInterface {
 		return -1;
 	}
 
-	private static void validate_args(String [] args) {
+	private static void validateArgs(String [] args) {
 		if (args.length != 2) {
-			System.out.println("Usage: java AdditionServer <server ip> <num players>");
+			System.out.println("Usage: java AdditionServer <server> <num players>");
 			System.exit(1);
 		}
 
@@ -73,49 +79,54 @@ public class Jogo extends UnicastRemoteObject implements JogoInterface {
 		}
 
 		try {
-			String server = "rmi://" + host + ":3000/Jogo";
-			Naming.rebind(server, new Jogo());
-			System.out.println("RmiGame server is ready.");
+			String remoteHost = "rmi://" + host + ":3000/Jogo";
+			Naming.rebind(remoteHost, new Jogo());
+			System.out.println("RmiGame server is ready.\n^C to quit...");
 		} catch (Exception e) {
 			System.out.println("Main thread failed: " + e);
+			e.printStackTrace();
 			System.exit(1);
 		}
 	}
 
 	public static void main(String [] args) throws RemoteException {
 
-		validate_args(args);
+		validateArgs(args);
 		registerServer(args[0]);
 
 		isFull = false;
 		nextPlayer = 0;
 		players = new String[numPlayers];
 
+		System.out.println("waiting for players...");
 		while (!isFull) {
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
-				System.out.println("Error at sleep: " + e);
+				System.out.println("Error at main thread: " + e);
+				e.printStackTrace();
 			}
 		}
 
+		System.out.println("game started!");
 		heartBeat();
-		callInit();
+//		callInit();
 	}
 
 	private static void callInit() {
 		for (int i = 0; i < players.length; i++) {
-			String conexao = "rmi://" + players[i] + ":3000/Jogador";
+			String remoteConnection = "rmi://" + players[i] + ":3001/Jogador";
 			new Thread() {
 				@Override
 				public void run() {
 					try{
-						System.out.println("Initiating heartbeat thread for: " + conexao);
-						JogadorInterface jogador = (JogadorInterface) Naming.lookup(conexao);
+						System.out.println("initiating callInit thread for: " + remoteConnection);
+						JogadorInterface jogador = (JogadorInterface) Naming.lookup(remoteConnection);
 						jogador.inicia();
 						this.interrupt();
 					} catch ( RemoteException | NotBoundException | MalformedURLException e) {
 						System.out.println("Exception at heartBeat: " + e);
+						e.printStackTrace();
 						this.interrupt();
 					}
 				}
@@ -125,16 +136,18 @@ public class Jogo extends UnicastRemoteObject implements JogoInterface {
 
 	private static void heartBeat() {
 		for (int i = 0; i < players.length; i++) {
-			String conexao = "rmi://" + players[i] + ":3000/Jogador";
+			String remoteConnection = "rmi://" + players[i] + ":3001/Jogador";
 			new Thread() {
 				@Override
 				public void run() {
 					try {
-						System.out.println("Initiating heartbeat thread for: " + conexao);
-						JogadorInterface jogador = (JogadorInterface) Naming.lookup(conexao);
+						System.out.println("initiating heartbeat thread for: " + remoteConnection);
+						JogadorInterface jogador = (JogadorInterface) Naming.lookup(remoteConnection);
+						System.out.println("blah");
 						while (true) {
 							if (jogador != null) {
 								jogador.cutuca();
+								System.out.println(remoteConnection + " is alive!");
 							} else {
 								this.interrupt();
 							}
@@ -142,7 +155,7 @@ public class Jogo extends UnicastRemoteObject implements JogoInterface {
 						}
 					}
 					catch( RemoteException | InterruptedException | NotBoundException | MalformedURLException e) {
-						System.out.println("Exception at heartBeat: " + e);
+						System.out.println("Connection to '" + remoteConnection + "' was lost!");
 						this.interrupt();
 					}
 				}
